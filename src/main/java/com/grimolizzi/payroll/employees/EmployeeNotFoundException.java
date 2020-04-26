fdd7ce1a0f8c9bc0f6174f85880ef289fee3f2a1
agrimolizzi <adriano.grimolizzi@amadeus.com>
2020-04-26 15:25:59 +0200

    add comment explaining Exception

--- src/main/java/com/grimolizzi/payroll/employees/EmployeeNotFoundException.java
+++ src/main/java/com/grimolizzi/payroll/employees/EmployeeNotFoundException.java
@@ -1,5 +1,7 @@
 package com.grimolizzi.payroll.employees;
 
+// This Exception extends RuntimeException: so it's  unchecked.
+// Note that the controller method that uses it doesn't throw it.
 public class EmployeeNotFoundException extends RuntimeException {
 
     public EmployeeNotFoundException(Long id) {
